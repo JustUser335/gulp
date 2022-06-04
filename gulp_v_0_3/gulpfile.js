f807225fa6229bb122c9b7e55f665a6e7e1e00b7
Stns <20976786+JustUser335@users.noreply.github.com>
2022-06-05 02:52:21 +0300

    gulp lessons

---
+++ gulp_v_0_3/gulpfile.js
@@ -0,0 +1,63 @@
+const imagemin = require('imagemin');
+const img_jpg  = require('imagemin-jpegtran')
+const img_png  = require('imagemin-pngquant')
+const img_webp = require('imagemin-webp')
+
+const gulp = require('gulp');
+
+
+/*===============================================*/
+
+gulp.task('hello',function () {
+    console.log('console otput');
+});
+gulp.task('log',function () {
+    console.log('console otput 1');
+});
+gulp.task('default1',gulp.parallel('hello','log'));
+
+/*===============================================*/
+
+// gulp.task('build1',function () {
+//     return gulp.src(['template/**'],{dot: true,nodir: true}).pipe(gulp.dest('prodaction'));
+// })
+// gulp.task('build2', function () {
+//     return gulp.src('tmp/**').pipe(gulp.dest('prodaction/about'))
+// })
+// gulp.task('def', gulp.parallel('build1','build2'))
+
+/*\
+|*| прям полезные
+|*| src
+|*| - dot: true добавляет файлы с точкой
+|*| - nodir: true игнорирует пустые папки
+|*| - read: можноигнорить файлы и собирать структуры
+|*| dest
+|*| - append обновляет содержимое файла
+|*| - sourcemaps плагин
+|*|
+\*/
+/*===============================================*/
+
+// gulp.task('build3',function () {
+//     return gulp.src(['template/*.html']).pipe(gulp.symlink('prodaction'));
+// })
+
+/*===============================================*/
+/*\
+|*| Объединение файлов в один чекнуить
+|*| сжатие изображений
+\*/
+
+gulp.task('build4',function () {
+    return gulp.src(['template/*.html'])
+        .pipe(imagemin(['images/*.{jpg,png}'], {
+            destination: 'build/images',
+            plugins: [
+                img_jpg(),
+                img_png(),
+                img_webp({quality: 50})
+            ])
+        .pipe(gulp.dest('prodaction'));
+})
+
---
+++ gulp_v_0_4/gulpfile.js
@@ -0,0 +1,93 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat      = require('gulp-concat');
+const uglify      = require('gulp-uglify-es').default;
+const sass        = require('gulp-sass')(require('sass'));
+const cssPrefix   = require('gulp-autoprefixer');
+const cleanCss    = require('gulp-clean-css');
+const imgMin      = require('compress-images');
+const del         = require('del');
+
+
+function browser_sync()
+{
+    browserSync.init({
+        server: { baseDir: 'app/' },
+        notify: true,
+        online: false
+    });
+}
+
+function scripts()
+{
+    return src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'app/js/app.js'
+    ])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js/'))
+    .pipe(browserSync.stream());
+}
+
+function styles()
+{
+    return src(['app/sass/main.sass','app/css/app.css'])
+        .pipe(sass())
+        .pipe(concat('app.min.css'))
+        .pipe(cssPrefix({overrideBrowserslist: ['last 10 versions'], greed: true}))
+        .pipe(cleanCss({ level: { 1: { specialComments: 0 }}}))
+        .pipe(dest('app/css/'))
+        .pipe(browserSync.stream())
+}
+
+function images()
+{
+    return Promise.resolve(
+        imgMin(
+            "app/images/src/**/*",
+            "app/images/dest/",
+            { compress_force: false, statistic: true, autoupdate: true },
+            false,
+            { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+            { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+            { svg: { engine: "svgo", command: "--multipass" } },
+            {
+                gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+            },
+            function (err, completed) {
+                if (completed === true) {
+                    browserSync.reload()
+                }
+            }
+        )
+    );
+}
+
+function updateImages() 
+{
+    return del(['app/images/dest/**']);
+}
+
+function watchAllFile()
+{
+    watch(['app/**/*.js','!app/**/*.min.js'],scripts);
+    watch(['app/sass/main.sass','app/css/app.css'], styles);
+    watch(['app/*.html']).on('change', browserSync.reload);
+    watch(['app/images/**'], images);
+}
+
+function prodaction_v_1_0()
+{
+    return src(['app/**/*','!app/images/src/**','!app/sass/**'])
+        .pipe(dest('dist/'));
+}
+
+exports.browser_sync = browser_sync;
+exports.scripts      = scripts;
+exports.styles       = styles;
+exports.images       = images;
+exports.updateImages = updateImages;
+
+exports.default      = parallel( scripts, styles, images, browser_sync, watchAllFile );
+exports.prodaction   = series( scripts, styles, images, prodaction_v_1_0);
